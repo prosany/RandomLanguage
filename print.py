280cf2ea8535cdbb312e121430a68afa92a3ad29
Md. Mahabub Hasan Sany <74856571+prosany@users.noreply.github.com>
2021-06-25 02:06:50 +0600

    Update print.py

--- print.py
+++ print.py
@@ -3,3 +3,29 @@ if 5 > 2:
 
 #print("Hello, World!")
 print("Cheers, Mate!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#print("Hello, World!")
+print("Cheers, Mate!")
